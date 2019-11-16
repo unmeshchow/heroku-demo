959d2ac319d8f8e58d796d0375c543f19fa4eb40
Unmesh Chowdhury <unmeshchow@gmail.com>
2019-11-16 17:51:53 +0600

    Initial commit

---
+++ src/test/java/com/unmeshc/herokudemo/HerokuDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unmeshc.herokudemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerokuDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

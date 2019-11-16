eaa1de64af4e0dea49e948cc63fcd77a93d4bd31
Unmesh Chowdhury <unmeshchow@gmail.com>
2019-11-16 20:18:55 +0600

    Changed id generation strategy to IDENTITY

--- src/main/java/com/unmeshc/herokudemo/domain/Country.java
+++ src/main/java/com/unmeshc/herokudemo/domain/Country.java
@@ -21,7 +21,7 @@ import javax.persistence.Id;
 public class Country {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 }
